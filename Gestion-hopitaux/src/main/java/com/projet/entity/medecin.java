95d9e8065d00c6ddf99a8c86cdc8556cd59d2049
kenabou <kenzaboueddaira22@gmail.com>
2020-12-27 18:10:28 +0100

    kenza test

--- Gestion-hopitaux/src/main/java/com/projet/entity/medecin.java
+++ Gestion-hopitaux/src/main/java/com/projet/entity/medecin.java
@@ -1,10 +1,7 @@
 package com.projet.entity;
 
 public class medecin {
-	private String Salah;
-	private String meryem;
-	private String kenza3;
-	private String kenza2;
+	private String Salah5;
 
 
 }
